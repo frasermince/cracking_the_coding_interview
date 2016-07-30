e12c1f4cac3082f546f215018222a54ab28c9520
Fraser Mince <frasermince@gmail.com>
2016-07-30 14:12:02 -0500

    Add first exercise.

---
+++ ChapterOne/unique.rb
@@ -0,0 +1,22 @@
+def unique(word)
+  word.chars.each_with_index do |letter, index|
+    if index != 0
+      word[0..index - 1].chars.each do |compared_letter|
+        if compared_letter == letter
+          return false
+        end
+      end
+    end
+  end
+  true
+end
+
+def test(word, expected_value)
+  puts "***#{word} is unique is #{unique(word)} expected #{expected_value}"
+end
+
+test 'MISSISSIPPI', false
+test 'CAMERON', true
+test 'Interview', false
+test 'fraser', false
+test 'tim', true
